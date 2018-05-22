11c41e841e35f6279e2739c3aa731b0f59842df8
Seth Schwartz <sschwartz214@gmail.com>
2018-05-22 10:54:45 -0600

    changes layout of grid to array of arrays

--- lib/board.rb
+++ lib/board.rb
@@ -2,10 +2,14 @@ class Board
   attr_reader :grid
 
   def initialize
-    @grid = [ [{"A1" => Space.new("A1")}, {"A2" => Space.new("A2")}, {"A3" => Space.new("A3")}, {"A4" => Space.new("A4")}],
-              [{"B1" => Space.new("B1")}, {"B2" => Space.new("B2")}, {"B3" => Space.new("B3")}, {"B4" => Space.new("B4")}],
-              [{"C1" => Space.new("C1")}, {"C2" => Space.new("C2")}, {"C3" => Space.new("C3")}, {"C4" => Space.new("C4")}],
-              [{"D1" => Space.new("D1")}, {"D2" => Space.new("D2")}, {"D3" => Space.new("D3")}, {"D4" => Space.new("D4")}]
+    @grid = [ [Space.new("A1"), Space.new("A2"), Space.new("A3"), Space.new("A4")],
+              [Space.new("B1"), Space.new("B2"), Space.new("B3"), Space.new("B4")],
+              [Space.new("C1"), Space.new("C2"), Space.new("C3"), Space.new("C4")],
+              [Space.new("D1"), Space.new("D2"), Space.new("D3"), Space.new("D4")]
             ]
+  end
+
+  def coordinates
+    A1 = grid[0][0]
   end
 end
--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,8 @@
 class Player
 
-  def place_ship(ship, row, column)
-    board.grid[row][column] = ship
-    ship.start_space = "A1"
-    ship.end_space = "B1"
+  def place_ship(ship, start_space, end_space)
+    ship.place(start_space, end_space)
+    board.grid[start_space][end_space] = ship
   end
 
 end
