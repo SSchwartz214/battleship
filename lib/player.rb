8ad531a7c94af208bf8ba2b4247a392c26baf34a
Seth Schwartz <sschwartz214@gmail.com>
2018-05-22 22:22:58 -0600

    adds place ship method

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,7 @@
 class Player
 
   def place_ship(ship, start_space, end_space)
-    ship.place(start_space, end_space)
+    ship.place[start_space][end_space]
     board.grid[start_space][end_space] = ship
   end
 
--- test/player_test.rb
+++ test/player_test.rb
@@ -3,5 +3,14 @@ require './lib/player'
 
 class PlayerTest < Minitest::Test
   def test_it_can_place_a_ship
-    
+    player_1 = Player.new
+    destroyer = Ship.new(2)
+
+    ship = destroyer
+    start_space = [0]
+
+    player.place_ship(destroyer)
+
+    assert_equal destroyer, board.grid[0][0]
+  end
 end
